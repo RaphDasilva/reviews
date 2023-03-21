115e33a0b6547291f61822051678d50ddfa0a1d7
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-03-21 21:08:03 +0530

    Add items.js

---
+++ src/components/Items.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Review from './Review';
+
+const Items = () => (
+  <main>
+    <section className="container">
+      <div className="title">
+        <h2>
+          Our reviwers
+        </h2>
+        <div className="underline" />
+
+      </div>
+      <Review />
+    </section>
+  </main>
+);
+
+export default Items;
